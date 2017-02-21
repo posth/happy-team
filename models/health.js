c9671216264b674e92b17cb95053dc248cdabead
Julien Moreau <julien.moreau@desjardins.com>
2017-02-21 15:29:13 -0500

    CappedCollection for health

--- models/health.js
+++ models/health.js
@@ -3,9 +3,17 @@ var Schema = mongoose.Schema;
 
 var User = require('./user');
 
-var schema = new Schema({
-    currentHealth: {type: Number, required: true},
-    user: { type: Schema.Types.ObjectId, ref: 'User'}
-});
+var schema = new Schema(
+    {
+        currentHealth: {type: Number, required: true},
+        user: { type: Schema.Types.ObjectId, ref: 'User'}
+    },
+    {
+        capped: {
+            size: 1024,
+            max: 100
+        }
+    }
+);
 
 module.exports = mongoose.model('Health', schema);
